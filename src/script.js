541babb5c807341b8f507e0687bf66704cc8ef83
Lucy Nowland <lucynowland03@gmail.com>
2023-12-22 09:33:15 +0000

    Added basic form handling and Typewriter JS Plugin

--- src/script.js
+++ src/script.js
@@ -1,1 +1,14 @@
+function generateRecommendation(event) {
+  event.preventDefault();
+  let recommendationContainer = document.querySelector("#book-recommendation");
 
+  new Typewriter("#book-recommendation", {
+    strings: "Book Recommendation",
+    autoStart: true,
+    delay: 30,
+    cursor: "",
+  });
+}
+
+let formElement = document.querySelector("#recommendation-generator-form");
+formElement.addEventListener("submit", generateRecommendation);
